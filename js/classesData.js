009e99d0c84653d4925750fc1da23ec4f0971ec4
Paveul <piwekpawel@gmail.com>
2026-05-30 16:28:03 +0200

    improve readability of the abstracts

--- js/classesData.js
+++ js/classesData.js
@@ -3,7 +3,11 @@ const classesData = [
     id: 'class-posets',
     title: 'Posets & Galois Connections',
     subtitle: 'Maths Beyond Limits, Falenty',
-    summary: 'All around mathematics there are examples of partial orders. We study these structures and the basic tools used to analyze them, with extensions to lattices, poset dimension, ordinals, and Möbius inversion, culminating with Galois connections as a unifying framework for dualities.',
+    summary: `
+    All around mathematics there are examples of partial orders.
+    We study these structures and the basic tools used to analyze them,
+    with extensions to lattices, poset dimension, ordinals, and Möbius inversion,
+    culminating with Galois connections as a unifying framework for dualities.`,
     driveLink: 'https://drive.google.com/file/d/13uU_JqTdlCPgOboMfCDvnUCstI-SbQux/view?usp=drive_link',
     tags: [
       { text: 'MBL 2025', link: 'https://mathsbeyondlimits.eu/poland/mbl-2025/' }
@@ -13,7 +17,11 @@ const classesData = [
     id: 'class-exotic-cousins',
     title: 'Exotic Cousins of Integers',
     subtitle: 'Maths Beyond Limits, Falenty',
-    summary: 'Ever got angry that there is no way to factorize $x^2+y^2$? Come to the dark side, where we treat $i = \\sqrt{-1}$ as if it were an integer. This class introduces the algebraic language of rings needed to explore these exotic \'cousins\' of integers and understand unique factorization.',
+    summary: `
+    Ever got angry that there is no way to factorize $x^2+y^2$?
+    Come to the dark side, where we treat $i = \\sqrt{-1}$ as if it were an integer.
+    This class introduces the algebraic language of rings needed to explore these
+    exotic 'cousins' of integers and understand unique factorization.`,
     driveLink: 'https://drive.google.com/file/d/1TazeZSOCpUJN8OueXj_sLMvKXr2FG50V/view?usp=sharing',
     tags: [
       { text: 'MBL 2024', link: 'https://mathsbeyondlimits.eu/poland/mbl-2024/' }
@@ -23,7 +31,10 @@ const classesData = [
     id: 'class-symmetry',
     title: 'What is Symmetry, Actually?',
     subtitle: 'Maths Beyond Limits, Falenty',
-    summary: 'An exploration of symmetry from a mathematical perspective, understanding the foundational concepts of groups and their actions through geometric examples like the tetrahedron and solving the cubic equation.',
+    summary: `
+    An exploration of symmetry from a mathematical perspective,
+    understanding the foundational concepts of groups and their actions
+    through geometric examples like the tetrahedron and solving the cubic equation.`,
     driveLink: 'https://drive.google.com/file/d/1vrs3Rbr0rfE2WRMvndzo_wE2hJKfS3sc/view?usp=sharing',
     tags: [
       { text: 'MBL 2020', link: 'https://mathsbeyondlimits.eu/poland/mbl-2020/' },
@@ -34,7 +45,11 @@ const classesData = [
     id: 'class-markov-chains',
     title: '(PL) Łańcuchy Markowa',
     subtitle: 'Maths Beyond Limits, Falenty',
-    summary: 'Które pole w Monopoly jest odwiedzane najczęściej? Dlaczego po losowym spacerze w 1D i 2D zawsze wrócimy do domu — ale w 3D możemy błąkać się wiecznie? Wprowadzenie do łańcuchów Markowa od podstaw aż do algorytmu PageRank.',
+    summary: `
+    Które pole w Monopoly jest odwiedzane najczęściej?
+    Dlaczego po losowym spacerze w 1D i 2D zawsze wrócimy do domu —
+    ale w 3D możemy błąkać się wiecznie? Wprowadzenie do łańcuchów Markowa
+    od podstaw aż do algorytmu PageRank.`,
     driveLink: 'https://drive.google.com/file/d/1bfHZdHtpggbfNFXs-q3-IC9bDG3SULar/view?usp=drive_link',
     tags: [
       { text: 'Falenty 2026', link: 'https://fundusz.org/fundusz-zdolni-rekrutacja/obozy/' }
